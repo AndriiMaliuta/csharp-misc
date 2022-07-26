b2d6f38d29d059c80fcac0f02104382a4be9b7bc
andrii maliuta <quadr988"gmail.com>
2022-07-26 19:59:00 +0300

    init

---
+++ Misc1.cs
@@ -0,0 +1,32 @@
+﻿namespace chasrp;
+
+public class Misc1
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new[] { 1, 2, 3 };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+
+        Point p1 = new Point(1, 2);
+        var x1 = p1.X;
+        var y1 = p1.Y;
+        
+        Array.Fill(arr,4);
+        
+        Console.WriteLine(arr);
+        Console.WriteLine(x1);
+        Console.WriteLine(y1);
+    }
+}
+
+public class Point
+{
+    public int X { get; }
+    public int Y { get; }
+    
+    public Point(int x, int y) => (X, Y) = (x, y);
+}
