42253cfceba1cb346647ccbdc39e1eaf9d977df4
andrii maliuta <quadr988"gmail.com>
2022-07-26 18:07:20 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.ComponentModel;
+using Eliot.UELib.dll;
+// using Memory;
+
+// [DllImport(@"Build_x64\Memory.dll")]
+// static extern void mpscExit();
+
+[DllImport(@"Eliot.UELib.dl")]
+UnrealLoader loader;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Process hmm = Process.GetProcessById(23792); //"MMH7Game-Win64-Shipping"
+        Console.WriteLine(hmm);
+    
+    // mpscExit();
+    //Mem m = new Mem();
+
+        var package = UnrealLoader.LoadFullPackage( "<PATH_TO_PACKAGE>", System.IO.FileAccess.Read );
+    
+        foreach( UObject obj in package.Objects )
+        {
+        Console.WriteLine( "Name: {0}, Class: {1}, Outer: {2}", obj.Name, obj.Class.Name, obj.Outer.Name );
+        }
+
+    }
+}
